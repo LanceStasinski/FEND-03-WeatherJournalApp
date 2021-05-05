200951e20fea985d1479cf7aebae08429ac432ba
Lance Stasinski <57465065+LanceStasinski@users.noreply.github.com>
2021-05-05 16:13:08 -0400

    node.js server up and running
    get and post routes now work. When a user enters information and submits, data is requested from an API, then that data is sent to the server and combined with the user's data. The full data object is sent back to the client and new elements are displayed on the web page.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //set endpoint
-projectData = {};
+projectData = [];
 
 //load express to run the server and routes
 const express = require('express');
@@ -10,7 +10,9 @@ const app = express();
 //load dependencies
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json);
+app.use(bodyParser.json());
+
+//cors - cross-origin allowance
 const cors = require('cors');
 app.use(cors());
 
@@ -18,11 +20,15 @@ app.use(cors());
 app.use(express.static('website'));
 
 //start server
-const port = 8000;
+const port = 3000;
 const server = app.listen(port, listening);
   function listening () {
     console.log(`running on localhost: ${port}`);
   };
+
+app.get('/', function (req, res) {
+  res.sendFile('website/index.html')
+})
 
 //GET route
 app.get('/all', sendData);
@@ -32,9 +38,18 @@ function sendData (req, res) {
 };
 
 //POST route
-app.post('/add', callBack);
 
-function callBack (req, res) {
-  res.send('Post recieved');
+app.post('/add', addEntry);
+
+function addEntry (req, res) {
+  console.log(req.body);
+  const newEntry = {
+    date: req.body.date,
+    temp: req.body.temp,
+    weather: req.body.weather,
+    feeling: req.body.feelings,
+    entry: req.body.entry,
+  };
+  projectData.push(newEntry);
 }
 
--- website/app.js
+++ website/app.js
@@ -1,30 +1,73 @@
 // OpenWeatherMap credentials
-let baseURL = 'api.openweathermap.org/data/2.5/weather?zip='
-let apiKey = '568e2956508e063f36a0424b221e7e3e';
-
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip='
+let apiKey = ',us&appid=3fdaddc8476ec856adc0078f023adbd7';
+//let zip = '04468';
+let entryId = 1;
 
 //POST data to local server
+const postData = async (url = '', data = {})=>{
+  const response = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {'Content-Type': 'application/json',},
+    body: JSON.stringify(data),
+  });
 
-//GET data from local server
+  try {
+    const newData = await response.json();
+    return newData;
+  } catch(error) {
+    console.log("error", error);
+    alert("Cannot post to server");
+  }
+}
 
+//GET data from the local server
+const getData = async (url='')=>{
+  const req = await fetch(url);
+  try {
+    const fullData = await req.json();
+    console.log(fullData);
 
+    document.getElementById('date').innerHTML = fullData[0].date;
+    document.getElementById('temp').innerHTML = "Temperature: " + fullData[0].temp;
+    document.getElementById('weather').innerHTML = "Weather: " + fullData[0].weather;
+    document.getElementById('thoughts').innerHTML = "Feelings: " + fullData[0].feeling;
 
+  } catch(error) {
+    console.log("error", error);
+    alert("Cannot connect to server");
+  }
+}
 
 //update the UI - need to have get and post functions first
 const generate = document.getElementById('generate');
-generate.addEventListener('click', updateJournal());
-function updateJournal () {
+generate.addEventListener('click', updateJournal);
+
+function updateJournal() {
   const zipCode = document.getElementById('zip').value;
   const feeling = document.getElementById('feeling').value;
-
+  const date = new Date();
+  const dateString = date.toDateString();
   //get weather from web api using input zip code
-  getWeather(baseURL, zipCode, apiKey);
+  getWeather(baseURL, zipCode, apiKey,)
+  .then(function(data) {
+    const req = {
+      date: dateString,
+      temp: data.main.temp,
+      weather: data.weather[0].main,
+      feelings: feeling,
+      entry: entryId,
+    };
+    entryId += 1;
+    postData('/add', req);
 
+  })
+  .then(getData('/all'))
 }
 
-//function that gets weather from the web api
-const getWeather = async (baseURL, zip, key)=>{
-  const res = await fetch(baseURL+zip+key);
+const getWeather = async (webURL, zip, key)=>{
+  const res = await fetch(webURL+zip+key);
   try {
     const data = await res.json();
     return data;
@@ -33,3 +76,4 @@ const getWeather = async (baseURL, zip, key)=>{
     alert("Weather for that zip code is unavailable");
   }
 }
+
